08efb387585fefec561d60caf95926b4146d13cb
Piotr Kuziak <piotrkz422@gmail.com>
2018-04-19 13:40:41 +0200

    Update dynamic

--- multiplymatrices.cpp
+++ multiplymatrices.cpp
@@ -2,8 +2,25 @@
 
 int multiplyMatrices(int amount_of_matrices, int** matrix_sizes, int*** matrix_array) {
 
+    int **matrix_multiplied, rows = 0, columns = 0;
+
     for(int i = 0; i < amount_of_matrices; i++) {
         if(matrix_sizes[i][1] != matrix_sizes[i + 1][0]) return ERR_MATRICES_WRONG_SIZE;
+    }
+
+    rows = matrix_sizes[0][0];
+    columns = matrix_sizes[amount_of_matrices - 1][1];
+
+    matrix_multiplied = new int* [rows];
+    for(int i = 0; i < matrix_sizes[0][0]; i++)
+        matrix_multiplied[i] = new int [columns];
+
+    for(int i  = 0; i < rows; i++) {
+        for(int j = 0; j < columns; j++) matrix_multiplied[i][j] = 0;
+    }
+
+    for(int i = 0; i < amount_of_matrices; i++) {
+
     }
 
     return OK;
